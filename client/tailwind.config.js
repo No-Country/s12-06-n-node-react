e0099465813e39230d3c7919e5eea7ca9599b0a4
scarletvargas <scarletvf47@gmail.com>
2023-12-02 17:12:33 -0400

    Agregando react-router-dom, fuente y colores en tailwind.config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,12 +2,21 @@
 export default {
 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				sans: ["DM sans"],
+			},
+			colors: {
+				principal: "#F2C81B",
+				secundario: "#FEFCF1",
+				texts: "#180801",
+			},
+		},
 		screens: {
-			'tablet': '375px',
-			'desktop': '744px',
-			'desktop-lg': '1440px',
-		}
+			tablet: "375px",
+			desktop: "744px",
+			"desktop-lg": "1440px",
+		},
 	},
 	plugins: [],
-}; 
+};
