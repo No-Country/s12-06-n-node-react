ee8b037bd395d55d9798a29c2eaf43d1ce6e2508
Gabriel Mendoza <mendozagabriel726@gmail.com>
2023-12-03 01:16:24 -0500

    feat:categoryCard v1.0 creada y agregada al layout

--- client/src/routes/auth/AuthRoutes.jsx
+++ client/src/routes/auth/AuthRoutes.jsx
@@ -1,11 +1,11 @@
-import { Navigate, Route, Routes } from "react-router-dom"
-import LoginPage from "../../pages/login"
+import { Navigate, Route, Routes } from "react-router-dom";
+import LoginPage from "../../pages/login";
 
 export const AuthRoutes = () => {
-    return (
-        <Routes>
-            <Route path="*" element={<Navigate to="/auth/login" />} />
-            <Route path="login" element={<LoginPage />} />
-        </Routes>
-    )
-}
+	return (
+		<Routes>
+			<Route path="*" element={<Navigate to="/auth/login" />} />
+			<Route path="login" element={<LoginPage />} />
+		</Routes>
+	);
+};
