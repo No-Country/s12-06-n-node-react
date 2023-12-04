cbc4445bc1877c4d6ff0ef53a9403e8f53ce6bff
Gabriel Mendoza <mendozagabriel726@gmail.com>
2023-12-04 15:04:09 -0500

    fix: pull issue

--- client/src/routes/Layout.jsx
+++ client/src/routes/Layout.jsx
@@ -8,25 +8,25 @@ export const Layout = () => {
 	return (
 		<div>
 			<Navbar />
-            <div className="flex flex-row">
-                <CategoryCard
-                    href={"/"}
-                    imgSrc={"https://dummyimage.com/150x150/000/fff"}
-                    className={"navbar-brand"}
-                >
-                    Pizzeria
-                </CategoryCard>
+			<div className="flex flex-row">
+				<CategoryCard
+					href={"/"}
+					imgSrc={"https://dummyimage.com/150x150/000/fff"}
+					className={"navbar-brand"}
+				>
+					Pizzeria
+				</CategoryCard>
 
-                <CategoryCard
-                    href={"/"}
-                    imgSrc={"https://dummyimage.com/150x150/000/fff"}
-                    className={"navbar-brand"}
-                >
-                    Parrilla
-                </CategoryCard>
+				<CategoryCard
+					href={"/"}
+					imgSrc={"https://dummyimage.com/150x150/000/fff"}
+					className={"navbar-brand"}
+				>
+					Parrilla
+				</CategoryCard>
 
-                <BottomBar />
-            </div>
+				<BottomBar />
+			</div>
 
 			<Outlet />
 			<Footer />
