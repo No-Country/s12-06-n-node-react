6352a1cff0f41ceff7e15c6857c94b314c011a1d
Gabriel Mendoza <mendozagabriel726@gmail.com>
2023-12-04 14:06:39 -0500

    feat: bottom bar hecha, agregada al layout del home

--- client/src/routes/Layout.jsx
+++ client/src/routes/Layout.jsx
@@ -2,6 +2,7 @@ import { Outlet } from "react-router-dom";
 import Footer from "../components/footer";
 import Navbar from "../components/navbar";
 import CategoryCard from "../components/categoryCard";
+import BottomBar from "../components/bottomBar";
 
 export const Layout = () => {
 	return (
@@ -23,6 +24,8 @@ export const Layout = () => {
                 >
                     Parrilla
                 </CategoryCard>
+
+                <BottomBar />
             </div>
 
 			<Outlet />
