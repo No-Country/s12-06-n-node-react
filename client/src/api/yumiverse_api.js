ca4dae6c764cb4b314167146e5c41fe03160393d
Jose-Puente2001 <60881496+Jose-Puente2001@users.noreply.github.com>
2023-12-04 20:08:08 -0400

    Arreglo del error de las funciones

--- client/src/api/yumiverse_api.js
+++ client/src/api/yumiverse_api.js
@@ -4,14 +4,14 @@ export const getRestaurant = async () => {
 	return await axios.get("http://localhost:3000/api/v1/restaurant");
 };
 
-export const addRestaurant = async data => {
+export const addRestaurant = async (data) => {
 	return await axios.post("http://localhost:3000/api/v1/restaurant", data);
 };
 
-export const updateRestaurant = async id => {
+export const updateRestaurant = async (id) => {
 	return await axios.put(`http://localhost:3000/api/v1/restaurant/${id}`);
 };
 
-export const deleteRestaurant = async id => {
+export const deleteRestaurant = async (id) => {
 	return await axios.delete(`http://localhost:3000/api/v1/restaurant/${id}`);
 };
