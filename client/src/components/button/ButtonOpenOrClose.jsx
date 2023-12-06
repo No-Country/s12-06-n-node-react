1399f3fd4e60de133f6d47d32e8d38331187b602
scarletvargas <scarletvf47@gmail.com>
2023-12-06 18:18:49 -0400

    Cambios en el buton de estado

--- client/src/components/button/ButtonOpenOrClose.jsx
+++ client/src/components/button/ButtonOpenOrClose.jsx
@@ -3,8 +3,8 @@ export default function ButtonOpenOrClose({ openRestaurant, disabled }) {
 		<button
 			disabled={disabled}
 			className={`rounded ${
-				openRestaurant ? "bg-[#1BF261]" : "bg-[#F21F1B]"
-			} -1 px-1.5 text-[10px] text-white`}
+				openRestaurant ? "bg-[#1BF261] text-texts" : "bg-[#F21F1B] text-secundario"
+			} -1 px-1.5 text-[10px]`}
 		>
 			{openRestaurant ? "Abierto" : "Cerrado"}
 		</button>
