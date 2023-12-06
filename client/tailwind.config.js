9d23691a0127f2ae582bd9a5ec571a6772a7f8c2
scarletvargas <scarletvf47@gmail.com>
2023-12-05 21:35:15 -0400

    adding box shadow to tailwind configuration

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -11,6 +11,10 @@ export default {
 				secundario: "#FEFCF1",
 				texts: "#180801",
 			},
+			boxShadow: {
+				"3xl": "0 1px 2px 0 rgba(0, 0, 0, 0.25)",
+				"4xl": "0 1px 2px 1px rgba(254, 252, 241, 0.25)",
+			},
 		},
 		screens: {
 			tablet: "375px",
