f7f292551e5c4f5505481f0fd70791314fe13e49
Jose-Puente2001 <josegabrielpuente75@gmail.com>
2023-12-06 16:51:29 -0400

    creacion de la barra bar

--- client/src/pages/ratings/index.jsx
+++ client/src/pages/ratings/index.jsx
@@ -1,14 +1,17 @@
 import Button from "../../components/button/Button";
 import qualifyIcon from "../../assets/icons/qualify.svg";
 import Comments from "../../components/comments";
+import BarProgress from "../../components/comments/components/BarProgress";
+import starIcon from "../../assets/icons/star.svg";
 
 export default function Ratings() {
-	return (
-		<div className="flex flex-col p-4 gap-6">
-			<div className="flex gap-4">
-				<Button text="Calificar" icon={qualifyIcon} alt="qualify icon" yellow rounded />
-			</div>
-			<Comments />
-		</div>
-	);
+  return (
+    <div className="flex flex-col p-4 gap-4">
+      <div className="flex gap-4">
+        <BarProgress />
+        <Button text="Calificar" icon={qualifyIcon} alt="qualify icon" yellow rounded />
+      </div>
+      <Comments />
+    </div>
+  );
 }
