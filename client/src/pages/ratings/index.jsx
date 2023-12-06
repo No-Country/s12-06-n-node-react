8c291ff8479b725573d12b15ae3ae58ccd3d8480
Jose-Puente2001 <josegabrielpuente75@gmail.com>
2023-12-06 17:12:07 -0400

    terminado de la barra de calificaciones

--- client/src/pages/ratings/index.jsx
+++ client/src/pages/ratings/index.jsx
@@ -8,7 +8,7 @@ export default function Ratings() {
   return (
     <div className="flex flex-col p-4 gap-4">
       <div className="flex gap-4">
-       <div>
+       <div className="mt-5">
         <div className="flex justify-center">
            <img src={starIcon} alt="star icon" />
            <h1 className="font-semibold text-lg ml-2">3</h1>
@@ -16,7 +16,7 @@ export default function Ratings() {
         <h2 className="font-semibold">22 opiniones</h2>
         </div>
         <BarProgress />
-        <div>
+        <div className="mt-5">
         <Button text="Calificar" icon={qualifyIcon} alt="qualify icon" yellow rounded  />
         </div>
       </div>
