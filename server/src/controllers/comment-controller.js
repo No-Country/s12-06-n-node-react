cb732a0ac28b92129535dd75913de029cc8bbf20
Marvin Ramos <mr487223@gmail.com>
2023-12-10 15:08:00 -0600

    cambio de logica de codigo por aplicacion de nuevo middleware

--- server/src/controllers/comment-controller.js
+++ server/src/controllers/comment-controller.js
@@ -13,58 +13,38 @@ const CommentController = {
 		}
 	},
 
-	// async getCommentsByRestaurantId(req, res) {
-	// 	try {
-	// 		const id = req.params.id;
+	async getCommentsByRestaurantId(req, res) {
+		try {
+			const restaurantId = req.params.RestaurantId;
+			const body = { id: restaurantId };
 
-	// 		const result = commentPartialValidator({ id });
-	// 		if (result.error) {
-	// 			handleHttp(res, "ERROR_GET_COMMENT", result.error);
-	// 		}
+			const comment = await CommentService.getCommentsById(body);
 
-	// 		const comment = await CommentService.getCommentsById(result.data);
-	// 		if (comment.error) {
-	// 			handleHttp(res, "ERROR_GET_COMMENT", comment.error);
-	// 		}
+			return res.status(200).json(comment);
+		} catch (e) {
+			handleHttp(res, "ERROR_GET_COMMENT", e);
+		}
+	},
 
-	// 		return res.status(200).json(comment);
-	// 	} catch (e) {
-	// 		handleHttp(res, "ERROR_GET_COMMENT", e);
-	// 	}
-	// },
+	async getComments(req, res) {
+		try {
+			const comments = await CommentService.getComments();
+			return res.status(200).json(comments);
+		} catch (e) {
+			handleHttp(res, "ERROR_GET_COMMENT", e);
+		}
+	},
+	async update(req, res) {
+		try {
+			const commentId = req.params.CommentId;
+			const body = { commentId, ...req.body};
 
-	// async getComments(req, res) {
-	// 	try {
-	// 		const comments = await CommentService.getComments();
-	// 		if (comments.error) {
-	// 			handleHttp(res, "ERROR_GET_COMMENT", comments.error);
-	// 		}
-
-	// 		return res.status(200).json(comments);
-	// 	} catch (e) {
-	// 		handleHttp(res, "ERROR_GET_COMMENT", e);
-	// 	}
-	// },
-	// async update(req, res) {
-	// 	try {
-	// 		const id = req.params.id;
-	// 		const body = {id, ...req.body};
-
-	// 		const result = commentPartialValidator(body);
-	// 		if (result.error) {
-	// 			handleHttp(res, "ERROR_UPDATE_COMMENT", result.error);
-	// 		}
-
-	// 		const comment = await CommentService.updateComment(result.data);
-	// 		if (comment.error) {
-	// 			handleHttp(res, "ERROR_UPDATE_COMMENT", comment.error);
-	// 		}
-
-	// 		return res.status(200).json(comment);
-	// 	} catch (e) {
-	// 		handleHttp(res, "ERROR_UPDATE_COMMENT", e);
-	// 	}
-	// }
+			const comment = await CommentService.updateComment(body);
+			return res.status(200).json(comment);
+		} catch (e) {
+			handleHttp(res, "ERROR_UPDATE_COMMENT", e);
+		}
+	}
 };
 
 export default CommentController;
--- server/src/services/comment-service.js
+++ server/src/services/comment-service.js
@@ -2,35 +2,37 @@ import { CommentModel } from "../models/index.js";
 
 const CommentService = {
 	async createComment(body) {
-		const responseInsert = await CommentModel.create(body);
+		const comment = await CommentModel.create(body);
 
-		console.log("SERVICE CREATE COMMENT:", responseInsert);
-		return responseInsert;
+		console.log("SERVICE CREATE COMMENT:", comment);
+		return comment;
 	},
 
 	async getCommentsById(body) {
 		const { id } = body;
 		// Comentario de Victor: Marvin, se puede usar findById() acá?
-		const comments = await CommentModel.find({ restaurant_id: id });
+		// Comentario de Marvin: Victor, findById() solo se puede usar si el id es el _id de mongo, si es otro campo no se puede usar :D
+		const comment = await CommentModel.find({ restaurantId: id });
 
-		console.log("SERVICE GET COMMENT:", comments);
-		return {
-			error: false,
-			message: "SERVICE GET COMMENT",
-			// data: comments, // remover
-		};
+		console.log("SERVICE GET COMMENT:", comment);
+		return comment;
 	},
 
-	async getComments(body) {
+	async getComments() {
 		const comments = await CommentModel.find();
 
 		console.log("SERVICE GET COMMENT:", comments);
-		return {
-			error: false,
-			message: "SERVICE GET COMMENT",
-			// data: comments, // remover
-		};
+		return comments;
 	},
+	async updateComment(body){
+		const { commentId, comment } = body;
+		const text = comment;
+
+		const commentUpdated = await CommentModel.findOneAndUpdate({ _id: commentId }, { comment: text }, { new: true });
+		
+		console.log("SERVICE UPDATE COMMENT:", commentUpdated);
+		return commentUpdated;
+	}
 };
 
 export default CommentService;
