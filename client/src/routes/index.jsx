74aa31bc3375327c5a8d5478de9bb0a8c8fdaec4
Eynar Alvarez <eynar.j.alvarez.m@gmail.com>
2023-12-12 01:41:11 -0400

    Register products route has been fixed.

--- client/src/routes/index.jsx
+++ client/src/routes/index.jsx
@@ -44,7 +44,7 @@ export const appRouter = createBrowserRouter([
 				element: <RestaurantPage />
 			},
 			{
-				path: "register",
+				path: "registerProducts",
 				element: <RegisterPage />
 			},
 		]
