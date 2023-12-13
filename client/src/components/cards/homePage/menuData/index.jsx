0ed8bf06899468d3a4b5af436762ba589bcff51e
scarletvargas <scarletvf47@gmail.com>
2023-12-13 12:26:38 -0400

    Adding id to MenuData component

--- client/src/components/cards/homePage/menuData/index.jsx
+++ client/src/components/cards/homePage/menuData/index.jsx
@@ -3,7 +3,16 @@ import { useState } from "react";
 import ContactSection from "./components/ContactSection";
 import PaymentsDeliverySection from "./components/PaymentsDeliverySection";
 
-export default function MenuData({ categories, phoneNumber, schedule, openRestaurant, location, stars, totalRatings }) {
+export default function MenuData({
+	id,
+	categories,
+	phoneNumber,
+	schedule,
+	openRestaurant,
+	location,
+	stars,
+	totalRatings,
+}) {
 	const [showContact, setShowContact] = useState(true);
 
 	const changeSection = isContact => {
@@ -27,6 +36,7 @@ export default function MenuData({ categories, phoneNumber, schedule, openRestau
 			</div>
 			{showContact ? (
 				<ContactSection
+					id={id}
 					categories={categories}
 					phoneNumber={phoneNumber}
 					schedule={schedule}
