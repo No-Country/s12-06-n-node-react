d356fe1af535cf6da37ee1121b1447c922b64ffa
scarletvargas <scarletvf47@gmail.com>
2023-12-13 12:31:21 -0400

    Updating ratings page with restaurant information on the number of stars and comments

--- client/src/pages/ratings/index.jsx
+++ client/src/pages/ratings/index.jsx
@@ -3,16 +3,33 @@ import qualifyIcon from "../../assets/icons/qualify.svg";
 import Comments from "../../components/comments";
 import BarProgress from "../../components/comments/components/BarProgress";
 import StarIcon from "../../icons/StarIcon";
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { getRestaurantById } from "../../api/yumiverse_api";
+import { useFetch } from "../../hooks/useFetch";
+import { useRestaurantStore } from "../../stores";
 
-export default function Ratings() {
+export default function RatingsPage() {
+	const { restaurantId } = useParams();
+
+	const setRestaurantName = useRestaurantStore(state => state.setRestaurantName);
+
+	const { data = {}, loading, error } = useFetch(() => getRestaurantById(restaurantId));
+	const { name, stars, totalRatings } = data;
+
+	useEffect(() => {
+		setRestaurantName(name);
+	}, [setRestaurantName, name]);
+
 	return (
 		<div className="flex items-center flex-col p-4 gap-6 min-w-[375px]">
 			<div className="flex gap-4 items-center">
 				<div>
-					<div className="flex justify-center">
-						<StarIcon className="fill-principal stroke-principal" /> <p className="text-xl ml-2">3</p>
+					<div className="flex justify-center items-center">
+						<StarIcon className="fill-principal stroke-principal h-5 w-5" />
+						<p className="text-xl ml-2">{stars}</p>
 					</div>
-					<h2 className="text-xs">22 opiniones</h2>
+					<h2 className="text-xs">{totalRatings} opiniones</h2>
 				</div>
 				<BarProgress />
 				<Button text="Calificar" icon={qualifyIcon} alt="qualify icon" yellow rounded />
