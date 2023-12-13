1c5b607772de9821ac467a4990bf6fd836625213
alejandro escudero <alekosescu@gmail.com>
2023-12-13 12:26:34 -0300

    validaciones_del_menu

--- server/src/routes/menu-route.js
+++ server/src/routes/menu-route.js
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import { MenuController } from "../controllers/index.js";
+import { menuValidation } from "../middlewares/index.js";
 
 const router = Router();
 
 router.get("/", MenuController.getMenus);
 router.get("/:id", MenuController.getMenu);
-router.post("/", MenuController.postMenu);
-router.put("/:id", MenuController.putMenu);
-router.delete("/:id", MenuController.deleteMenu);
+router.post("/", menuValidation.create, MenuController.postMenu);
+router.put("/:id", menuValidation.update, MenuController.putMenu);
+router.delete("/:id", menuValidation.delete, MenuController.deleteMenu);
 
 export default router;
