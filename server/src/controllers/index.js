f3cdfd539cbf1673e4bf3f4b9509669799e571fa
Marvin Ramos <mr487223@gmail.com>
2023-12-13 13:37:06 -0600

    agregando controladores

--- server/src/controllers/index.js
+++ server/src/controllers/index.js
@@ -2,5 +2,6 @@ import RestaurantController from "./restaurant-controller.js";
 import CategoryController from "./category-controller.js";
 import MenuController from "./menu-controller.js";
 import CommentController from "./comment-controller.js";
+import UserController from "./user-controller.js";
 
-export { RestaurantController, CategoryController, MenuController, CommentController };
+export { RestaurantController, UserController, CategoryController, MenuController, CommentController };
--- server/src/controllers/user-controller.js
+++ server/src/controllers/user-controller.js
@@ -1,11 +1,11 @@
-import { userService } from "../services/user-service.js";
+import { UserService } from "../services/index.js";
 import { handleHttp } from "../utils/error-handle.js";
 
 const UserController = {
     async create(req, res) {
         try {
             const body = req.body;
-            const user = await userService.createUser(body);
+            const user = await UserService.createUser(body);
 
             return res.status(200).json(user);
         } catch (e) {
