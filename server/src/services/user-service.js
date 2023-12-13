e8d192286948028c927b70791953ecdc0391fbca
Marvin Ramos <mr487223@gmail.com>
2023-12-13 14:32:17 -0600

    agregando encriptacion a contrasenias

--- server/src/services/user-service.js
+++ server/src/services/user-service.js
@@ -1,7 +1,14 @@
 import { UserModel } from "../models/index.js";
+import bcrypt from "bcryptjs";
 
 const UserService = {
     async createUser(body) {
+        const { password } = body;
+        
+        const salt = bcrypt.genSaltSync(10);
+        const hash = bcrypt.hashSync(password, salt);
+        
+        body.password = hash;
         const user = await UserModel.create(body);
 
         console.log("SERVICE CREATE USER:", user);
