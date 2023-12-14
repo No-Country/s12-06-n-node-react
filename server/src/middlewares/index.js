7634c7fb65ef2448839b5533d757e1c639f70892
varayac <victor.d.araya@gmail.com>
2023-12-14 11:54:16 -0300

    Se corrige error en middlewares/index.js

--- server/src/middlewares/index.js
+++ server/src/middlewares/index.js
@@ -2,6 +2,14 @@ import { logs } from "./log-middleware.js";
 import commentValidation from "./comment-middleware.js";
 import restaurantValidation from "./restaurant-middleware.js";
 import categoryValidation from "./category-middleware.js";
+import menuValidation from "./menu-middleware.js";
 import userValidation from "./user-middleware.js";
 
-export { logs, commentValidation, userValidation, restaurantValidation, categoryValidation };
+export {
+	logs,
+	commentValidation,
+	userValidation,
+	restaurantValidation,
+	categoryValidation,
+	menuValidation,
+};
