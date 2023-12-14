a3c8785803f711cf12c774fa5b87f81942dc2e61
Eynar Alvarez <eynar.j.alvarez.m@gmail.com>
2023-12-14 12:51:24 -0400

    Fixing the slider section in the HomePage.

---
+++ client/src/helpers/categoryIcons.js
@@ -0,0 +1,46 @@
+import ItalianaIcon from "../assets/categoryIcons/componentsIcons/italiana";
+import MexicanaIcon from "../assets/categoryIcons/componentsIcons/mexicana";
+import JaponesaIcon from "../assets/categoryIcons/componentsIcons/japonesa";
+import VegetarianaIcon from "../assets/categoryIcons/componentsIcons/vegetariana";
+import RapidaIcon from "../assets/categoryIcons/componentsIcons/rapida";
+import MariscosIcon from "../assets/categoryIcons/componentsIcons/mariscos";
+import CafeteriasIcon from "../assets/categoryIcons/componentsIcons/cafeterias";
+import SaludableIcon from "../assets/categoryIcons/componentsIcons/saludable";
+import CostenaIcon from "../assets/categoryIcons/componentsIcons/costena";
+import PastasIcon from "../assets/categoryIcons/componentsIcons/pastas";
+
+// import ItalianaIcon from "../assets/icons/classic.svg";
+// import MexicanaIcon from "../assets/icons/classic.svg";
+// import JaponesaIcon from "../assets/icons/classic.svg";
+// import VegetarianaIcon from "../assets/icons/classic.svg";
+// import RapidaIcon from "../assets/icons/classic.svg";
+// import MariscosIcon from "../assets/icons/classic.svg";
+// import CafeteriasIcon from "../assets/icons/classic.svg";
+// import SaludableIcon from "../assets/icons/classic.svg";
+// import CostenaIcon from "../assets/icons/classic.svg";
+// import PastasIcon from "../assets/icons/classic.svg";
+
+const categoryIcons = {
+    // italiana: ItalianaIcon,
+    // mexicana: MexicanaIcon,
+    // japonesa: JaponesaIcon,
+    // vegetariana: VegetarianaIcon,
+    // rapida: RapidaIcon,
+    // mariscos: MariscosIcon,
+    // cafeterias: CafeteriasIcon,
+    // saludable: SaludableIcon,
+    // costena: CostenaIcon,
+    // pastas: PastasIcon,
+    "656d3f15b996334e3f2424f5": ItalianaIcon,
+    "656d3f51b996334e3f2424f7": MexicanaIcon,
+    "656d3f5eb996334e3f2424f9": JaponesaIcon,
+    "656d3f6ab996334e3f2424fb": VegetarianaIcon,
+    "656d3fa8b996334e3f2424fd": RapidaIcon,
+    "656d3fb9b996334e3f242501": MariscosIcon,
+    "656d3fc6b996334e3f242503": CafeteriasIcon,
+    "656d3fd0b996334e3f242505": SaludableIcon,
+    "6573c977ef822e84d3938adc": CostenaIcon,
+    "657760841bf9d20de731b010": PastasIcon,
+}
+
+export default categoryIcons;
