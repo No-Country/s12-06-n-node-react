44c2df608774f32c3ed880e889a7cce3eff67716
Marvin Ramos <mr487223@gmail.com>
2023-12-15 14:05:09 -0600

    agg logica de validacion jwt

---
+++ server/src/middlewares/token-middleware.js
@@ -0,0 +1,22 @@
+import jwt from 'jsonwebtoken';
+
+const tokenValidation = (req, res, next) => {
+    const token = req.header('x-access-token');
+    if(!token) return res.status(401).json({ message: "No token provided" });
+    
+    try {        
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.userId = decoded.id;
+
+    } catch (error) {
+        if(error instanceof jwt.TokenExpiredError) {
+            return res.status(401).json({ message: "Token expired" });
+        }
+        if(error instanceof jwt.JsonWebTokenError) {
+            return res.status(401).json({ message: "Invalid token" });
+        }
+        return res.status(401).json({ message: "Unauthorized" });
+    }
+};
+
+export default tokenValidation;
