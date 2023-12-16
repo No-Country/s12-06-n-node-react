b891dfad6c3c8e4a2918a639caa75f7deddadc5e
scarletvargas <scarletvf47@gmail.com>
2023-12-16 15:12:29 -0400

    fixing eslint errors

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,8 +1,7 @@
 import React from "react";
 import { appRouter } from "./routes/index.jsx";
 import ReactDOM from "react-dom/client";
-// import App from "./App.jsx";
-import { BrowserRouter, RouterProvider } from "react-router-dom";
+import { RouterProvider } from "react-router-dom";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
