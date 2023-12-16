2ad1f9acdeabdb92d1d6fea090b19a95457398b0
Marvin Ramos <mr487223@gmail.com>
2023-12-16 14:30:25 -0600

    agregando decodificaciones

--- server/src/routes/user-route.js
+++ server/src/routes/user-route.js
@@ -5,13 +5,9 @@ import { userValidation } from "../middlewares/index.js";
 const router = Router();
 
 router.post("/register", userValidation.register, UserController.create);
-// actualizar usuario por id de usuario
 router.patch("/update/:id", userValidation.update, UserController.update);
+
 // agregar restaurante por id de usuario
 router.post("/:id", userValidation.addRestaurant, UserController.createRestaurant);
-// agregar favorito
-
-// router.post("/login", userValidation.login, UserController.login);
 
 export default router;
-// create & edit user
