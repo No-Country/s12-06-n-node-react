41daf816510fa70d8b7749e525ec3ceb324abbcc
scarletvargas <scarletvf47@gmail.com>
2023-12-20 19:20:19 -0400

    actualizando la api de front

--- client/src/api/yumiverse_api.js
+++ client/src/api/yumiverse_api.js
@@ -27,6 +27,14 @@ export const postUser = async (name, surname, phone, username, password) => {
 	);
 };
 
-export const loginUser = async(username, password) => {
-	return await axios.post("http://localhost:3000/API/v1/user/login", username, password)
-}
+export const loginUser = async (username, password) => {
+	return await axios.post("https://yumi-verse.onrender.com/api/v1/user/login", username, password);
+};
+
+export const getCommentsByIdRestaurant = async idRestaurant => {
+	return await axios.get(`https://yumi-verse.onrender.com/api/v1/comment/${idRestaurant}`);
+};
+
+export const getUserById = async idUser => {
+	return await axios.get(`http://localhost:3000/api/v1/user/${idUser}`);
+};
